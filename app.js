7f394b35ed5cd084672f5c1e74a85977d523c466
slabwu <134193743+slabwu@users.noreply.github.com>
2025-12-01 17:29:19 +0800

    Connect session to Postgres database

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const path = require('node:path')
 const session = require('express-session')
+const pgSession = require('connect-pg-simple')(session)
+const pg = require('pg')
 const indexRoute = require('./routes/index')
 require('dotenv').config()
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+})
 
 const assetsPath = path.join(__dirname, 'public')
 app.use(express.static(assetsPath))
@@ -11,11 +17,23 @@ app.use(session({
   secret: process.env.KEY,
   resave: false,
   saveUninitialized: true
-}));
+}))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  store: new pgSession({
+    pool: pool,
+    tableName: 'session',
+  }),
+  secret: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  }
+}))
 
 app.use('/', indexRoute)
 
