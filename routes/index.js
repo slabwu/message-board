f03000af1c58feb7178f3704e16ac6db0d9b60bb
slabwu <134193743+slabwu@users.noreply.github.com>
2025-12-01 15:26:04 +0800

    Remove new & message routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const index = Router()
 
 index.get('/', messageController.getIndex)
 index.post('/', messageController.postNew)
-index.get('/new', messageController.getNew)
-index.post('/new', messageController.postNew)
-index.get('/message/:messageId', messageController.getMessageId)
+// index.get('/new', messageController.getNew)
+// index.post('/new', messageController.postNew)
+// index.get('/message/:messageId', messageController.getMessageId)
 
 module.exports = index
