86e4cbfe1adb7995781567f1a1ac575c3de5d483
slabwu <134193743+slabwu@users.noreply.github.com>
2025-12-01 14:10:23 +0800

    Force SSL

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,12 @@ require('dotenv').config()
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  },
+  keepAlive: true,
+  connectionTimeoutMillis: 5000
 })
 
 module.exports = pool
